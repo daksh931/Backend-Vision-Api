ee19150a7ef91a349cd8228e94fe5eb6d6dc46fe
Daksh <goyaldaksh5522@gmail.com>
2024-05-27 16:38:52 +0530

    changes logout

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/logout",logout);
+router.get("/logout", isAuthorized,logout);
 router.get("/details", isAuthorized,getUserDetails);
 
 router.put("/password/reset/:userid/:token",resetPassword);
